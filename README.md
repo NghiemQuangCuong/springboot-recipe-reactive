a8058f45b140f34f4ea737ece405aa1919a7749f
CuongNghiem <quangcuongnghiem@gmail.com>
2021-09-23 16:23:46 +0700

    Init Project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # springboot-recipe
+
 Spring Boot Recipe
---
+++ src/test/java/com/cuongnghiem/springbootrecipe/SpringbootRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuongnghiem.springbootrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
