5cfe46a3d663f85f437e5f95940facf8e5f237dd
CuongNghiem <quangcuongnghiem@gmail.com>
2021-10-08 13:08:49 +0700

    Add Project Lombok, Write Unit test, using Mockito.

---
+++ src/main/java/com/cuongnghiem/springbootrecipe/services/RecipeService.java
@@ -0,0 +1,13 @@
+package com.cuongnghiem.springbootrecipe.services;
+
+import com.cuongnghiem.springbootrecipe.model.Recipe;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+}
